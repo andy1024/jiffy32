d2202acc47b2ccc12e5e4c3f28f47d125c8a9cac
andy1024 <andrzej.maslowski1024@gmail.com>
2015-12-28 16:28:38 +0100

    minor refactoring

--- jiffy32/src/main/java/org/warheim/interfacing/jiffy32/hl/I2C_PCF8591.java
+++ jiffy32/src/main/java/org/warheim/interfacing/jiffy32/hl/I2C_PCF8591.java
@@ -2,10 +2,10 @@ package org.warheim.interfacing.jiffy32.hl;
 
 import com.codeminders.hidapi.ClassPathLibraryLoader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.warheim.interfacing.jiffy32.FF32Factory;
 import org.warheim.interfacing.jiffy32.FF32c;
 import org.warheim.interfacing.jiffy32.Pin;
 import org.warheim.interfacing.jiffy32.exceptions.ArgumentException;
@@ -17,25 +17,24 @@ import org.warheim.interfacing.jiffy32.exceptions.JiffyException;
  *
  * @author andy
  */
-public class I2C_PCF8591 {
-    Logger logger = LoggerFactory.getLogger(I2C_PCF8591.class);
+public class I2C_PCF8591 extends I2CDevice {
+    private static final Logger logger = LoggerFactory.getLogger(I2C_PCF8591.class);
     private static final int SLAVE_ADDRESS = 0x48;
     
     static {
         ClassPathLibraryLoader.loadNativeHIDLibrary();
     }
     
-    final private FF32c jiffy;
-    private Map<String, Integer> ports;
+    final private Map<String, Integer> ports;
     
     public I2C_PCF8591(FF32c jiffy, Pin SCL, Pin SDA, Map<String, Integer> ports) throws IOException, JiffyException, ArgumentException {
-        this.jiffy = jiffy;
+        super(jiffy, SCL, SDA, SLAVE_ADDRESS);
         this.ports = ports;
-        jiffy.setI2CPins(SCL, SDA);
     }
     
     public I2C_PCF8591(Pin SCL, Pin SDA) throws IOException, JiffyException, ArgumentException {
-        this(FF32Factory.getInstance(), SCL, SDA, null);
+        super(SCL, SDA, SLAVE_ADDRESS);
+        this.ports = new HashMap<>();
     }
 
     public int readPort(String portName) throws IOException, JiffyException, ArgumentException {
@@ -47,8 +46,8 @@ public class I2C_PCF8591 {
     }
 
     public int readPort(int portNumber) throws IOException, JiffyException, ArgumentException {
-        jiffy.writeByteI2C(SLAVE_ADDRESS, portNumber);
-        int retval = jiffy.readByteI2C(SLAVE_ADDRESS);
+        writeByteI2C(portNumber);
+        int retval = readByteI2C();
         return retval;
     }
     
